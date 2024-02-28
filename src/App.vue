f957964d593da43d0dea2a92c76ebc268c09fc5f
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-02-28 17:20:02 +0700

    Setup TransactionList props

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,23 @@
   <div class="container">
     <Balance></Balance>
     <IncomeExpenses></IncomeExpenses>
-    <TransactionList></TransactionList>
+    <TransactionList :transactions="transactions"></TransactionList>
     <AddTransaction></AddTransaction>
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+import { ref } from "vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
-};
+const transactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 35 },
+  { id: 3, text: "Book", amount: -90 },
+  { id: 4, text: "Cameraxxx", amount: 40 },
+];
 </script>
