e32ac086f28c3b820fb843710876c40fd638579c
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-02-28 11:27:52 +0700

    Import incomeExpenses component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <Header></Header>
   <div class="container">
     <Balance></Balance>
+    <IncomeExpenses></IncomeExpenses>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
 
 export default {
   components: {
     Header,
     Balance,
+    IncomeExpenses,
   },
 };
 </script>
