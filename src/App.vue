a1cb262a92c76dd5efb0bebb9fc15df50b0c71c7
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-02-28 17:32:42 +0700

    Setup Balance props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header></Header>
   <div class="container">
-    <Balance></Balance>
+    <Balance :total="total"></Balance>
     <IncomeExpenses></IncomeExpenses>
     <TransactionList :transactions="transactions"></TransactionList>
     <AddTransaction></AddTransaction>
@@ -14,12 +14,18 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-const transactions = [
+const transactions = ref([
   { id: 1, text: "Flower", amount: -20 },
   { id: 2, text: "Salary", amount: 35 },
   { id: 3, text: "Book", amount: -90 },
   { id: 4, text: "Cameraxxx", amount: 40 },
-];
+]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction)=> {
+    return acc + transaction.amount;
+  }, 0);
+});
 </script>
