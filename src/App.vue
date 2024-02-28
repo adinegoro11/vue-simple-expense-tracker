56c6d96fa03227873c5ace2e24f6062df988f4b8
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-02-28 11:20:46 +0700

    Setup Header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  my App
+  <Header></Header>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
